1dd0186ca020116fcce69b94ca03e9870dbb03e8
ADRIELYSSON DARLYSON DA SILVA E SILVA <adrielysson.darlyson@academico.ifrn.edu.br>
2022-10-19 16:19:06 -0300

    Criando Repository para Evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,13 +1,17 @@
 package ifrn.pi.eventos.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.EventoRepository;
 
 @Controller
 public class EventosController {
+	@Autowired
+	private EventoRepository er;
 	
 	@RequestMapping("/eventos/forms")
 	public String Form() {
@@ -16,6 +20,7 @@ public class EventosController {
 	@PostMapping("/eventos/adicionar")
 	public String add(Evento dados) {
 	System.out.println(dados);
+	er.save(dados);
 		return "adicionar";
 	}
 }
