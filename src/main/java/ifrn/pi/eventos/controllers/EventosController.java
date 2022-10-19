6276edd1e9628bbd9fff78d5c06c4f711e2883ce
ADRIELYSSON DARLYSON DA SILVA E SILVA <adrielysson.darlyson@academico.ifrn.edu.br>
2022-10-19 15:54:20 -0300

    Enviando dados do formulário para a aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,13 +1,21 @@
 package ifrn.pi.eventos.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
 
 @Controller
 public class EventosController {
 	
-	@RequestMapping("/eventos/form")
+	@RequestMapping("/eventos/forms")
 	public String Form() {
 		return "formEvento";
+	}
+	@PostMapping("/eventos/adicionar")
+	public String add(Evento dados) {
+	System.out.println(dados);
+		return "adicionar";
 	}
 }
